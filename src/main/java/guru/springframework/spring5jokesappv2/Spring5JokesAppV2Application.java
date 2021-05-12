1caf9702e124cb66cc2068983f96e82ef683bd1b
Nazry Luey <nluey@greshamtech.com>
2021-05-12 14:28:42 +0800

    Add template to show joke and fix package in app

--- src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
+++ src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -1,4 +1,4 @@
-package guru.springframework.chucknorrisjoke;
+package guru.springframework.spring5jokesappv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
